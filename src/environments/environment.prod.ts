575d08eb8291c783345e2e62c52094ef48c137e8
Toni <tonidalmases@gmail.com>
2020-08-16 17:51:26 +0200

    remove firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,5 @@ export const environment = {
   production: true,
 
   firebase: {
-    apiKey: 'AIzaSyA0eYQGzI16-CaJUOK4r2SXTuwGvR8A_MA',
-    authDomain: 'e-shop-62efa.firebaseapp.com',
-    databaseURL: 'https://e-shop-62efa.firebaseio.com',
-    projectId: 'e-shop-62efa',
-    storageBucket: 'e-shop-62efa.appspot.com',
-    messagingSenderId: '351186702391',
-    appId: '1:351186702391:web:5010cdc1956a1ae7ea2fb2',
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,6 @@ export const environment = {
   production: false,
 
   firebase: {
-    apiKey: 'AIzaSyA0eYQGzI16-CaJUOK4r2SXTuwGvR8A_MA',
-    authDomain: 'e-shop-62efa.firebaseapp.com',
-    databaseURL: 'https://e-shop-62efa.firebaseio.com',
-    projectId: 'e-shop-62efa',
-    storageBucket: 'e-shop-62efa.appspot.com',
-    messagingSenderId: '351186702391',
-    appId: '1:351186702391:web:5010cdc1956a1ae7ea2fb2',
   },
 };
 
